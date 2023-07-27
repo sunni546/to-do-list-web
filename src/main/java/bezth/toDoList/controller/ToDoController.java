6a156b125831e8339eafa2340d580fe9140c2aec
Sunhee Kim <sunni372@icloud.com>
2023-07-27 21:00:47 +0900

    변수 이름 수정

--- src/main/java/bezth/toDoList/controller/ToDoController.java
+++ src/main/java/bezth/toDoList/controller/ToDoController.java
@@ -13,8 +13,8 @@ public class ToDoController {
     private final ToDoService toDoService = new ToDoService();
 
     @PostMapping("/todos")  // '할 일' 추가
-    public void createToDo(@RequestBody HashMap<String, String> mapToDO)  {
-        toDoService.createToDo(mapToDO);
+    public void createToDo(@RequestBody HashMap<String, String> mapToDo)  {
+        toDoService.createToDo(mapToDo);
     }
 
     @GetMapping("/todos")    // '할 일' 목록 조회
@@ -24,8 +24,8 @@ public class ToDoController {
 
     @PatchMapping("/todos/{id}")  // '할 일' 수정
     public void updateToDo(@PathVariable("id") int id,
-                           @RequestBody HashMap<String, Optional<Object>> mapToDO) {
-        toDoService.updateToDo(id, mapToDO);
+                           @RequestBody HashMap<String, Optional<Object>> mapToDo) {
+        toDoService.updateToDo(id, mapToDo);
     }
 
     @DeleteMapping("/todos/{id}")  // '할 일' 삭제
--- src/main/java/bezth/toDoList/service/ToDoService.java
+++ src/main/java/bezth/toDoList/service/ToDoService.java
@@ -12,9 +12,9 @@ public class ToDoService {
     private final ToDo_SQLite toDoSqLite = new ToDo_SQLite();
     private final Account_SQLite accountSqLite = new Account_SQLite();
 
-    public void createToDo(HashMap<String, String> mapToDO)  {
-        int account_pk = accountSqLite.findPK(mapToDO.get("ID"), mapToDO.get("PW"));
-        toDoSqLite.insertDB(mapToDO.get("title"), mapToDO.get("content"), account_pk);
+    public void createToDo(HashMap<String, String> mapToDo)  {
+        int account_pk = accountSqLite.findPK(mapToDo.get("ID"), mapToDo.get("PW"));
+        toDoSqLite.insertDB(mapToDo.get("title"), mapToDo.get("content"), account_pk);
     }
 
     public JSONObject readToDo(HashMap<String, String> mapAccount) {
@@ -22,9 +22,9 @@ public class ToDoService {
         return toDoSqLite.selectDB(account_pk);
     }
 
-    public void updateToDo(int id, HashMap<String, Optional<Object>> mapToDO) {
-        int account_pk = accountSqLite.findPK(mapToDO.get("ID").get().toString(), mapToDO.get("PW").get().toString());
-        toDoSqLite.updateDB(id, mapToDO.get("title"), mapToDO.get("content"), mapToDO.get("done"), account_pk);
+    public void updateToDo(int id, HashMap<String, Optional<Object>> mapToDo) {
+        int account_pk = accountSqLite.findPK(mapToDo.get("ID").get().toString(), mapToDo.get("PW").get().toString());
+        toDoSqLite.updateDB(id, mapToDo.get("title"), mapToDo.get("content"), mapToDo.get("done"), account_pk);
     }
 
     public void deleteToDo(int id, HashMap<String, String> mapAccount) {
