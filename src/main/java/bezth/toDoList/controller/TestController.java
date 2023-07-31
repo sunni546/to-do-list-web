05b66ec8be71c84871fa4f0e7cfaf11fe324ab7a
Sunhee Kim <sunni372@icloud.com>
2023-07-31 22:16:22 +0900

    Update TestController.java

--- src/main/java/bezth/toDoList/controller/TestController.java
+++ src/main/java/bezth/toDoList/controller/TestController.java
@@ -1,7 +1,6 @@
 package bezth.toDoList.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
