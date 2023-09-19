94312e07b1652b6ffc875f45a0e4d9a6340da3e7
Sunhee Kim <sunni372@icloud.com>
2023-09-19 22:58:00 +0900

    Access Token인지 확인

--- src/main/java/bezth/toDoList/service/ToDoService.java
+++ src/main/java/bezth/toDoList/service/ToDoService.java
@@ -4,7 +4,9 @@ import bezth.toDoList.jwt.JwtTokenProvider;
 import bezth.toDoList.database.ToDo_SQLite;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONObject;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Optional;
@@ -18,24 +20,34 @@ public class ToDoService {
     private final JwtTokenProvider jwtTokenProvider;
 
     public void createToDo(HashMap<String, String> mapToDo, String authorization)  {
-        // 인증
+        IsAccessToken(authorization);
         int account_pk = jwtTokenProvider.findAccountPkByJwt(authorization);
         toDoSqLite.insertDB(mapToDo.get("title"), mapToDo.get("content"), account_pk);
     }
 
     public JSONObject readToDo(String authorization) {
+        IsAccessToken(authorization);
         int account_pk = jwtTokenProvider.findAccountPkByJwt(authorization);
         return toDoSqLite.selectDB(account_pk);
     }
 
     public void updateToDo(int id, HashMap<String, Optional<Object>> mapToDo, String authorization) {
+        IsAccessToken(authorization);
         int account_pk = jwtTokenProvider.findAccountPkByJwt(authorization);
         toDoSqLite.updateDB(id, mapToDo.get("title"), mapToDo.get("content"), mapToDo.get("done"), account_pk);
     }
 
     public void deleteToDo(int id, String authorization) {
+        IsAccessToken(authorization);
         int account_pk = jwtTokenProvider.findAccountPkByJwt(authorization);
         toDoSqLite.deleteDB(id, account_pk);
+    }
+
+    // Access Token인지 확인
+    public void IsAccessToken(String authorization)  {
+        if (!jwtTokenProvider.findTokenTypeByJwt(authorization).equals("accessToken")) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);     // status code : 400
+        }
     }
 
     public JSONObject readAllToDo() {
